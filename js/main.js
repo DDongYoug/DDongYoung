1c87e39e13cec30cd8aa53285d73ff715e4507de
YukDDong <112530541+YukDDong@users.noreply.github.com>
2022-11-11 02:11:25 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -26,7 +26,8 @@ $(document).ready(function(){
         setTimeout(() => {
             moving_stop=false
             console.log(moving_stop)
-        }, 1200);
+            // clearInterval(skillAni)
+        }, 1000)
     }      
 
 
@@ -35,26 +36,49 @@ $(document).ready(function(){
             moving_stop=true
             console.log(moving_stop)
             $('#total_box>div').css('z-index','0')
-            $('.nav li').removeClass('on')
+            $('.nav li').removeClass('on').removeClass('on1')
             var currentEl=$('#total_box>div').eq(current)
             var nextEl=$('#total_box>div').eq(n)    
 
             currentEl.css('z-index','50')
             nextEl.css('z-index','30').css({left: '0', top: '0'})
             currentEl.css({top: '0'}).animate({top: '-100vh'},1000, )
-            
-            $('.nav li').eq(n).addClass('on')
-            
             // 첫화면 마지막화면 버튼 숨기기
-            if(n==0){
-                $('#move_btn .up_btn').hide()
-            } 
-            if(0<n<4){
-                $('#move_btn .down_btn').show()
-                $('#move_btn .up_btn').show()
-            }
-            if(n==4){
-                $('#move_btn .down_btn').hide()
+            // if(n==0){
+            //     $('#move_btn .up_btn').hide()
+            // } 
+            // if(0<n<4){
+            //     $('#move_btn .down_btn').show()
+            //     $('#move_btn .up_btn').show()
+            // }
+            // if(n==4){
+            //     $('#move_btn .down_btn').hide()
+            // }
+
+            switch(n){
+                case 0:
+                    $('#move_btn .up_btn').hide()
+                    $('#move_btn .down_btn').show()
+                    $('.nav li').eq(n).addClass('on')
+                    break
+
+                case 1: case 3:
+                    $('#move_btn .down_btn').show()
+                    $('#move_btn .up_btn').show()
+                    $('.nav li').eq(n).addClass('on')
+                    break
+
+                case 2:
+                    $('#move_btn .down_btn').show()
+                    $('#move_btn .up_btn').show()
+                    $('.nav li').eq(n).addClass('on1') 
+                    skillAni()
+                    break
+
+                case 4:
+                    $('#move_btn .down_btn').hide()
+                    $('#move_btn .up_btn').show()
+                    $('.nav li').eq(n).addClass('on1')
             }
 
             current=n
@@ -66,7 +90,7 @@ $(document).ready(function(){
         if(moving_stop==false){
             moving_stop=true
             $('#total_box>div').css('z-index','0')
-            $('.nav li').removeClass('on')
+            $('.nav li').removeClass('on').removeClass('on1')
             var currentEl=$('#total_box>div').eq(current)
             var nextEl=$('#total_box>div').eq(n)
 
@@ -74,21 +98,44 @@ $(document).ready(function(){
             nextEl.css('z-index','50')
             nextEl.css({top: '-100vh'}).stop().animate({top: '0'},1000)
             
-            
-            $('.nav li').eq(n).addClass('on')
-
             // 첫화면 마지막화면 버튼 숨기기
-            if(n==4){
-                $('#move_btn .down_btn').hide()
+            // if(n==4){
+            //     $('#move_btn .down_btn').hide()
+            // }
+            // if(1<=n<=3){
+            //     $('#move_btn .down_btn').show()
+            //     $('#move_btn .up_btn').show()
+            // }
+            // if(n==0){
+            //     $('.up_btn').hide()
+            //     console.log('ok')
+            // } 
+
+            switch(n){
+                case 0:
+                    $('#move_btn .up_btn').hide()
+                    $('#move_btn .down_btn').show()
+                    $('.nav li').eq(n).addClass('on')
+                    break
+
+                case 1: case 3:
+                    $('#move_btn .down_btn').show()
+                    $('#move_btn .up_btn').show()
+                    $('.nav li').eq(n).addClass('on')
+                    break
+
+                case 2:
+                    $('#move_btn .down_btn').show()
+                    $('#move_btn .up_btn').show()
+                    $('.nav li').eq(n).addClass('on1')
+                    skillAni()
+                    break
+
+                case 4:
+                    $('#move_btn .down_btn').hide()
+                    $('#move_btn .up_btn').show()
+                    $('.nav li').eq(n).addClass('on1')
             }
-            if(1<=n<=3){
-                $('#move_btn .down_btn').show()
-                $('#move_btn .up_btn').show()
-            }
-            if(n==0){
-                $('.up_btn').hide()
-                console.log('ok')
-            } 
             
             current=n
             setTimeoutMS()
@@ -120,5 +167,61 @@ $(document).ready(function(){
         }
     })
 
-    //스킬 상태바
+    //스킬
+
+    const skillAni=()=>{setTimeout(() => {
+        console.log('ok')
+            $('.chart').circleProgress({
+                size:200,
+                //그래프 크기
+                startAngle: -Math.PI/2 ,
+                //시작지점 (기본값 Math.PI)
+                thickness:30,
+                //그래프두께
+                fill: {gradient: ['#ff1e41', '#ff9f8e']},
+                //그래프 선 색
+                emptyFill: "rgba(0,0,0,0.3)",
+                //그래프 빈칸색 기본값 rgba(0,0,0,0.1)
+                fill: {gradient: ['#ff1e41', '#ff9f8e']},
+            })
+        
+            $('.chart1').circleProgress({
+                value: 0.9,
+                fill: {gradient: ['#ff1e41', '#ff9f8e']}
+            }).on('circle-animation-progress', function(event, progress) {
+                $(this).find('strong').html(Math.round(90 *progress) + '<i>%</i>');
+            });
+        
+            $('.chart2').circleProgress({
+                value: 0.8,
+                fill: {gradient: ['#ff1e41', '#ff9f8e']}
+            }).on('circle-animation-progress', function(event, progress) {
+                $(this).find('strong').html(Math.round(80 *progress) + '<i>%</i>');
+            });
+        
+            $('.chart3').circleProgress({
+                value: 0.8,
+                fill: {gradient: ['#ff1e41', '#ff9f8e']}
+            }).on('circle-animation-progress', function(event, progress) {
+                $(this).find('strong').html(Math.round(80 *progress) + '<i>%</i>');
+            });
+        
+            $('.chart4').circleProgress({
+                value: 0.7,
+                fill: {gradient: ['#ff1e41', '#ff9f8e']}
+            }).on('circle-animation-progress', function(event, progress) {
+                $(this).find('strong').html(Math.round(70 *progress) + '<i>%</i>');
+            });
+        
+            $('.chart5').circleProgress({
+                value: 0.1,
+                fill: {gradient: ['#ff1e41', '#ff9f8e']}
+            }).on('circle-animation-progress', function(event, progress) {
+                $(this).find('strong').html(Math.round(10 *progress) + '<i>%</i>');
+            });
+        
+    }, 1000);}
+            
+    
+
 })
